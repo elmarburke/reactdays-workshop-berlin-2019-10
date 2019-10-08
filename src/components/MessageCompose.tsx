5f90bf8cae4114d74962eb65e81ba9b618b02cd7
Elmar Burke <hallo@elmarburke.de>
2019-10-08 16:30:12 +0200

    state hook

--- src/components/MessageCompose.tsx
+++ src/components/MessageCompose.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {}
 
 const MessageCompose: React.FunctionComponent<Props> = () => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     console.log(["hip", "hip"]);
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
+      <input onChange={handleChange} />
       <button type="submit">✉ Send</button>
+      {inputValue}
     </form>
   );
 };
