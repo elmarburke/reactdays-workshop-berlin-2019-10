761c096bc3c8da50d1a9b03c726ecc2f761df0c2
Elmar Burke <hallo@elmarburke.de>
2019-10-08 10:58:34 +0200

    task 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hallo Berlin</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
