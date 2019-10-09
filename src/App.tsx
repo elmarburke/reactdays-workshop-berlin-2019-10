43ce7e834d6ed9aa209638b9f55aa864202f500a
Elmar Burke <hallo@elmarburke.de>
2019-10-09 10:30:37 +0200

    setState, lifting state up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import MessageList from "./components/MessageList";
 import MessageCompose from "./components/MessageCompose";
+import { Message } from "./domain/Message";
 
 function App(): JSX.Element {
+  const messages: Message[] = [
+    { id: "1", author: "Christian", message: "Guten Morgen", date: 42 },
+    {
+      id: "2",
+      author: "Elmar",
+      message: "Spreek me gerust vanavond aan",
+      date: 1982
+    }
+  ];
+
+  const [messageList, setMessageList] = useState(messages);
+
   return (
     <React.Fragment>
       <MessageCompose />
-      <MessageList />
+      <MessageList messages={messageList} />
     </React.Fragment>
   );
 }
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import { Message } from "../domain/Message";
 import MessageView from "./MessageView";
 
-const MessageList: React.FunctionComponent = () => {
-  const messages: Message[] = [
-    { id: "1", author: "Christian", message: "Guten Morgen", date: 42 },
-    {
-      id: "2",
-      author: "Elmar",
-      message: "Spreek me gerust vanavond aan",
-      date: 1982
-    }
-  ];
+interface Props {
+    messages: Message[];
+}
 
-  return (
+const MessageList: React.FunctionComponent<Props> = ({ messages }) => {
+   return (
     <React.Fragment>
       {messages.map(message => (
         <MessageView key={message.id} message={message} />
